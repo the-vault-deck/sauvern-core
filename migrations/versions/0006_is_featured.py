5805dd347d3b50c97633d5f100472fea52459f20
the-vault-deck <emailmainstsolutions@gmail.com>
2026-03-24 14:10:05 -0500

    fix: 0006 down_revision '0005_product_id' → '0005' (matches actual revision ID)

--- migrations/versions/0006_is_featured.py
+++ migrations/versions/0006_is_featured.py
@@ -1,14 +1,14 @@
 """add is_featured to listings
 
 Revision ID: 0006_is_featured
-Revises: 0005_product_id
+Revises: 0005
 Create Date: 2026-03-24
 """
 from alembic import op
 import sqlalchemy as sa
 
 revision = '0006_is_featured'
-down_revision = '0005_product_id'
+down_revision = '0005'
 branch_labels = None
 depends_on = None
 
